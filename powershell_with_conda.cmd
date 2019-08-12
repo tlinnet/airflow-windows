f3ef4040415161cd3834a591c5a43af33527cdd8
Troels Schwarz-Linnet <tlinnet@gmail.com>
2019-08-12 08:48:31 +0200

    Added powershell alias "winairflow" to execute airflow

--- powershell_with_conda.cmd
+++ powershell_with_conda.cmd
@@ -1,3 +1,3 @@
 REM start PowerShell -Noexit -Command ". 'C:\ProgramData\Miniconda3\shell\condabin\conda-hook.ps1';"
 REM start PowerShell -Noexit -Command ". 'C:\ProgramData\Miniconda3\shell\condabin\conda-hook.ps1'; conda activate 'C:\ProgramData\Miniconda3'"
-start PowerShell -Noexit -Command ". 'C:\ProgramData\Miniconda3\shell\condabin\conda-hook.ps1'; conda activate airflow"
+start PowerShell -Noexit -Command ". 'C:\ProgramData\Miniconda3\shell\condabin\conda-hook.ps1'; conda activate airflow; $AIRFLOW_SCRIPT = (where.exe airflow); function Do-Winairflow {python $AIRFLOW_SCRIPT}; Set-Alias -Name winairflow -Value Do-Winairflow"
